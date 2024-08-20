e0fe263183c6bff8b5985c58ff855090ad67b078
CarrotEasy0214 <seongmin0214@kakao.com>
2024-08-20 14:06:11 +0900

    edit:Swiper Test(1)

--- src/Components/Slide.tsx
+++ src/Components/Slide.tsx
@@ -26,10 +26,18 @@ const Slide = () => {
       modules={[Autoplay, Pagination, Navigation]}
       className="mySwiper"
     >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
+      <SwiperSlide>
+        <img src={slide1} alt="" className="slide" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={slide2} alt="" className="slide" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={slide3} alt="" className="slide" />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img src={slide4} alt="" className="slide" />
+      </SwiperSlide>
     </Swiper>
   );
 };
