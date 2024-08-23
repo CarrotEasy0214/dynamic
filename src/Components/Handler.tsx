6dad883a2bfde4d55bb59fa89f70e94e00cf4017
CarrotEasy0214 <seongmin0214@kakao.com>
2024-08-23 12:22:28 +0900

    edit:Swiper(ing)(2)

--- src/Components/Handler.tsx
+++ src/Components/Handler.tsx
@@ -1,19 +1,18 @@
 import { useCallback, useState } from "react";
 import { useSwiper } from "swiper/react";
+import Play from "./Play";
 
 const Handler = (): JSX.Element => {
   const swiper = useSwiper();
-  const [runinng, setRunning] = useState<string>("멈춤");
+  const [runinng, setRunning] = useState<string>("");
   const autoPlayer = useCallback(() => {
     swiper.autoplay.paused ? swiper.autoplay.resume() : swiper.autoplay.pause();
-    setRunning(swiper.autoplay.paused ? "시작" : "멈춤");
+    setRunning(swiper.autoplay.paused ? "" : "");
   }, [swiper.autoplay]);
   return (
-    <div className="flex justify-end relative top-[-5.3rem] z-50 pr-5">
-      <div
-        onClick={autoPlayer}
-        className="w-10 bg-white text-black text-center"
-      >
+    <div className="float-right relative top-[-5rem] z-30 pr-5">
+      <div onClick={autoPlayer} className="w-10">
+        <Play></Play>
         {runinng}
       </div>
     </div>
--- src/Components/Slide.tsx
+++ src/Components/Slide.tsx
@@ -2,13 +2,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 // import { PaginationOptions } from "swiper/types";
-import {
-  Autoplay,
-  Pagination,
-  Navigation,
-  EffectFade,
-  Parallax,
-} from "swiper/modules";
+import { Autoplay, Pagination, Navigation, EffectFade, Parallax } from "swiper/modules";
 
 import slide1 from "../img/slide/slide1.jpg";
 import slide2 from "../img/slide/slide2.jpg";
@@ -18,14 +12,12 @@ import Handler from "./Handler";
 
 const Slide = () => {
   const pagination = {
-    // clickable: true,
-    // renderBullet: function (index: number, className: string) {
-    //   console.log(className);
-    //   return '<span class="' + className + '"></span>';
-    // },
-    bulletClass:
-      "w-3 h-3 rounded-lg bg-slate-500 inline-block relative mr-4 bottom-20 float-right right-20",
+    clickable: true,
+    bulletClass: "w-3 h-3 rounded-lg bg-slate-500 inline-block relative mr-4 bottom-20",
     bulletActiveClass: "bg-white w-[3rem]",
+    renderBullet: function (index: any, className: string) {
+      return '<div class="flex justify-end pr-5"><span class="' + className + '"></span><div/ >';
+    },
   };
 
   return (
@@ -43,22 +35,19 @@ const Slide = () => {
     >
       <SwiperSlide>
         <div
-          className="absolute top-[75%] left-[8%] text-white text-5xl font-bold"
+          className="absolute top-[75%] left-[8%] text-white xl:text-6xl lg:text-5xl font-bold text-4xl"
           data-swiper-parallax="-300"
         >
           MW09 True Wireless Earphones
         </div>
-        <div
-          className="absolute top-[83%] left-[8%] text-white"
-          data-swiper-parallax="-200"
-        >
+        <div className="absolute top-[83%] left-[8%] text-white" data-swiper-parallax="-200">
           Our newest active noise-cancelling earphones
         </div>
         <div
-          className="absolute top-[89%] left-[8%] bg-white w-[8.5rem] h-[3.5rem] flex justify-center text-sm font-bold"
+          className="absolute top-[89%] left-[8%] bg-white w-[8.5rem] h-[3.5rem] flex justify-center text-sm font-bold cursor-pointer text-center items-center"
           data-swiper-parallax="-100"
         >
-          <button>Shop Now</button>
+          Show Now
         </div>
         <img src={slide1} alt="" className="slide" />
       </SwiperSlide>
@@ -70,17 +59,14 @@ const Slide = () => {
           <p>MG20 Wireless Gaming</p>
           <p>Headphones</p>
         </div>
-        <div
-          className="absolute top-[83%] left-[8%] text-white"
-          data-swiper-parallax="-200"
-        >
+        <div className="absolute top-[83%] left-[8%] text-white" data-swiper-parallax="-200">
           Never Play the Same
         </div>
         <div
-          className="absolute top-[89%] left-[8%] bg-white w-[8.5rem] h-[3.5rem] flex justify-center text-sm font-bold"
+          className="absolute top-[89%] left-[8%] bg-white w-[8.5rem] h-[3.5rem] flex justify-center text-sm font-bold cursor-pointer text-center items-center"
           data-swiper-parallax="-100"
         >
-          <button>Shop Now</button>
+          Show Now
         </div>
         <img src={slide2} alt="" className="slide" />
       </SwiperSlide>
@@ -92,38 +78,29 @@ const Slide = () => {
           <p>MW75 Active Noise-Cancelling</p>
           <p>Wireless Headphones</p>
         </div>
-        <div
-          className="absolute top-[83%] left-[8%] text-white"
-          data-swiper-parallax="-200"
-        >
+        <div className="absolute top-[83%] left-[8%] text-white" data-swiper-parallax="-200">
           Perfect silence, perfect sound
         </div>
         <div
-          className="absolute top-[89%] left-[8%] bg-white w-[8.5rem] h-[3.5rem] flex justify-center text-sm font-bold"
+          className="absolute top-[89%] left-[8%] bg-white w-[8.5rem] h-[3.5rem] flex justify-center text-sm font-bold cursor-pointer text-center items-center"
           data-swiper-parallax="-100"
         >
-          <button>Shop Now</button>
+          Show Now
         </div>
         <img src={slide3} alt="" className="slide" />
       </SwiperSlide>
       <SwiperSlide>
-        <div
-          className="absolute top-[75%] left-[8%] text-white text-5xl font-bold"
-          data-swiper-parallax="-300"
-        >
+        <div className="absolute top-[75%] left-[8%] text-white text-5xl font-bold" data-swiper-parallax="-300">
           MH40 Wireless Headphones
         </div>
-        <div
-          className="absolute top-[83%] left-[8%] text-white"
-          data-swiper-parallax="-200"
-        >
+        <div className="absolute top-[83%] left-[8%] text-white" data-swiper-parallax="-200">
           Iconic design and brilliant acoustics
         </div>
         <div
-          className="absolute top-[89%] left-[8%] bg-white w-[8.5rem] h-[3.5rem] flex justify-center text-sm font-bold"
+          className="absolute top-[89%] left-[8%] bg-white w-[8.5rem] h-[3.5rem] flex justify-center text-sm font-bold cursor-pointer text-center items-center"
           data-swiper-parallax="-100"
         >
-          <button>Shop Now</button>
+          Show Now
         </div>
         <img src={slide4} alt="" className="slide" />
       </SwiperSlide>
