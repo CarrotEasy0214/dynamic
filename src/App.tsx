cc8376727affd68b779c9c7e6509beb4c9fbf659
CarrotEasy0214 <seongmin0214@kakao.com>
2024-08-27 02:47:12 +0900

    edit:SwiperOptionError(ing)

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 // import "./App.css";
-import BlackContainer from "./Components/BlackContainer";
-import Card from "./Components/Card";
-import Collabo from "./Components/Collabo";
-import CompanyGuide from "./Components/CompanyGuide";
+// import BlackContainer from "./Components/BlackContainer";
+// import Card from "./Components/Card";
+// import Collabo from "./Components/Collabo";
+// import CompanyGuide from "./Components/CompanyGuide";
 // import Test from "./Components/example/Test";
-import MandD from "./Components/MandD";
+// import MandD from "./Components/MandD";
 import Menu from "./Components/Menu";
 import Notice from "./Components/Notice";
-import ProductPage from "./Components/ProductPage";
-import ShippingDetail from "./Components/ShippingDetail";
-import Slide from "./Components/Slide";
+// import ProductPage from "./Components/ProductPage";
+// import ShippingDetail from "./Components/ShippingDetail";
+// import Slide from "./Components/Slide";
 import Slide3 from "./Components/Slide3";
 // import Slide2 from "./Components/Slide2";
-import SupportCenter from "./Components/SupportCenter";
+// import SupportCenter from "./Components/SupportCenter";
 
 function App() {
   return (
